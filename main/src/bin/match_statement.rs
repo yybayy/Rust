c752d2a393f227231a6ab15ed06c305f266fb642
yybayy <yigitgrowtopa22@gmail.com>
2025-06-01 15:35:43 +0300

    04.1
    updated match statement

--- main/src/bin/match_statement.rs
+++ main/src/bin/match_statement.rs
@@ -5,4 +5,15 @@ fn main() {
         true => println!("Situation is true"),
         false => println!("Situation is false"),
     }
+    
+    let some_number = 5;
+    
+    match some_number {
+        1 => println!("One"),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        4 => println!("Four"),
+        5 => println!("Five"),
+        _ => println!("No one"),
+    }
 }
